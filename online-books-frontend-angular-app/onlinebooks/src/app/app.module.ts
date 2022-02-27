06353610bdba3dcb0ee2c304d95c977cd9ce019a
miriamwojcik <wk.miriam@gmail.com>
2022-02-27 18:19:17 +0000

    product management component structure

--- online-books-frontend-angular-app/onlinebooks/src/app/app.module.ts
+++ online-books-frontend-angular-app/onlinebooks/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { AuthService } from './services/auth.service';
+import { ProductmanagementComponent } from './components/productmanagement/productmanagement.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    ProductmanagementComponent
   ],
   imports: [
     BrowserModule,
