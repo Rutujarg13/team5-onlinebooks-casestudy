cb59d94e2a4b580e2f831c14f2c730008153d23e
miriamwojcik <wk.miriam@gmail.com>
2022-02-28 13:13:19 +0000

    product management endpoints updated

--- online-books-frontend-angular-app/onlinebooks/src/app/components/productmanagement/productmanagement.component.ts
+++ online-books-frontend-angular-app/onlinebooks/src/app/components/productmanagement/productmanagement.component.ts
@@ -9,8 +9,13 @@ import { PoductmanagementService } from 'src/app/services/poductmanagement.servi
 export class ProductmanagementComponent implements OnInit {
   productManagementService: PoductmanagementService;
   bookList:any[]=[];
+  publishers:any[]=[];
+  authors:any[]=[];
   booksAuthors:any[]=[];
+  categories:any[]=[];
   filter:string='';
+  bookId:any;
+  bookAuthors:any[]=[];
   constructor(service: PoductmanagementService) {
     this.productManagementService = service;
    }
@@ -19,18 +24,140 @@ export class ProductmanagementComponent implements OnInit {
     this.productManagementService.getAllBooks()
     .subscribe((response:any)=>{
       this.bookList = response;
+      console.log("Get all books");
       console.log(response);
     }
-    )
+    );
 
     this.productManagementService.getBooksAuthors()
     .subscribe((response:any)=>{
       this.booksAuthors = response;
+      console.log("Books Authors")
       console.log(response);
-    })
+    });
 
+    this.productManagementService.getCategories()
+    .subscribe((response:any)=>{
+      this.categories = response;
+      console.log("Categories")
+      console.log(response);
+    });
+
+    this.productManagementService.getPublishers()
+    .subscribe((response:any)=>{
+      this.publishers = response;
+      console.log("Publishers")
+      console.log(response);
+    });
+
+    this.productManagementService.getAuthors()
+    .subscribe((response:any)=>{
+      this.authors = response;
+      console.log("Authors");
+      console.log(response);
+    });
 }
+
+getAuthorsOfTheBook(bookId:any){
+  this.productManagementService.getBookAuthors(bookId)
+  .subscribe((response:any)=>{
+    this.bookAuthors = response;
+    console.log("Get Authors of the Book");
+    console.log(response);
+  });
+}
+
+deleteBooksAuthor(bookId:number, authorId:number){
+  let bookAuthor = {"book_id":bookId, "author_id":authorId};
+  this.productManagementService.deleteBooksAuthor(bookAuthor)
+  .subscribe((response:any)=>{
+    console.log("Delete Book's Author")
+    console.log(response);
+  });
+}
+
+deleteBook(){
+  this.productManagementService.deleteBook(this.bookId)
+  .subscribe((response:any)=>{
+    console.log("Delete Book")
+    console.log(response);
+  });
+}
+
+editBook(title:string, publisherId: number, price:number, quantity:number, description:string, categoryId:number, cover:string){
+let book ={
+  "book_id": this.bookId,
+    "title":title,
+      "publisher_id": publisherId,
+      "price":price,
+      "quantity":quantity,
+      "description":description,
+      "category_id":categoryId,
+      "cover":cover
+};
+  this.productManagementService.editBook(book)
+  .subscribe((response:any)=>{
+    console.log("Book Edited");
+    console.log(response);
+  });
+}
+
+//add book
+addBook(title:string, publisherId: number, price:number, quantity:number, description:string, categoryId:number, cover:string, authors:string[]){
+  let book ={
+      "title":title,
+        "publisher_id": publisherId,
+        "price":price,
+        "quantity":quantity,
+        "description":description,
+        "category_id":categoryId,
+        "cover":cover,
+        "authors":authors
+  };
+    this.productManagementService.addBook(book)
+    .subscribe((response:any)=>{
+      console.log("Book Added");
+      console.log(response);
+    });
+  }
+
+
+//add author
+addAuthor(firstName:string, lastName:string){
+let author = {
+  "firstName":"Ernest",
+  "lastName":"Hemingway"
+}
+this.productManagementService.addAuthor(author)
+.subscribe((response:any)=>{
+  console.log("Author Added");
+  console.log(response);
+});
+}
+
+//add publisher
+addPublisher(publisherName:string){
+  let publisher={ "publisher_name":publisherName};
+  this.productManagementService.addPublisher(publisher)
+.subscribe((response:any)=>{
+  console.log("Publisher Added");
+  console.log(response);
+});
+}
+
+
+//add category
+addCategory(categoryName:string){
+  let category={ "category_name":categoryName};
+  this.productManagementService.addPublisher(category)
+.subscribe((response:any)=>{
+  console.log("Category Added");
+  console.log(response);
+});
+}
+
 transformFilter(e:any){
   this.filter=e.target.value.toLowerCase();
 }
+
 }
--- online-books-frontend-angular-app/onlinebooks/src/app/services/poductmanagement.service.ts
+++ online-books-frontend-angular-app/onlinebooks/src/app/services/poductmanagement.service.ts
@@ -27,5 +27,40 @@ export class PoductmanagementService {
     return this.http.get("http://localhost:3000/api/admin/products/categories");
   }
 
+  public getPublishers(){
+    return this.http.get("http://localhost:3000/api/admin/products/publishers");
+  }
+
+  public getAuthors(){
+    return this.http.get("http://localhost:3000/api/admin/products/authors");
+  }
+
+ public editBook(book_details:any){
+   return this.http.put("http://localhost:3000/api/admin/products/edit", book_details);
+ }
+
+ public addBook(book_details:any){
+   return this.http.post("http://localhost:3000/api/admin/products/add", book_details);
+ }
+
+ public addAuthor(author_details:any){
+   return this.http.post("http://localhost:3000/api/admin/products/add/author", author_details);
+ }
+
+ public addPublisher(publisher_details:any){
+   return this.http.post("http://localhost:3000/api/admin/products/add/publisher", publisher_details);
+ }
+
+ public addCategory(category_details: any){
+   return this.http.post("http://localhost:3000/api/admin/products/add/category", category_details);
+ }
+
+ public deleteBooksAuthor(book_author:any){
+  return this.http.delete("http://localhost:3000/api/admin/products/deletebookauthor/", book_author);
+ }
+
+ public deleteBook(bookId:string){
+   return this.http.delete(`http://localhost:3000/api/admin/products/delete/${bookId}`);
+ }
   
 }
